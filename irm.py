d1199af284dc1fe7276231406853535e530ad43e
Abelo <abelo08cruz@gamil.com>
2022-05-08 23:45:35 -0400

    Finished model,need to test with docs

--- irm.py
+++ irm.py
@@ -1,18 +1,25 @@
+import numpy as np
 import math
 import re
 import string
+import os
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk import *
 from nltk.corpus import stopwords
 
 class Vector_Space_Model:
-    def __init__(self,corpus_size) -> None:
-        self.tf = {}
+    def __init__(self,corpus_size, path) -> None:
+        self.query_tf = {}
+        self.doc_tf = {}
+
         self.idf = {}
         self.invert_index = {}
-        self.wights = {}
+        self.doc_wights = {}
+        self.query_wights = {}
+        self.sim = {}
         self.corpus_size = corpus_size
+        self.corpus_path = path
 
 
     def __remove_reg__(self,tokens):
@@ -36,21 +43,28 @@ class Vector_Space_Model:
         # word to root word
         # porter = PorterStemmer()
         # tokens = [porter.stem(word) for word in tokens]
-
+        doc.close()
         return tokens
 
-    def calc_tf(self,tokens,dj):
-        for t in tokens:
-            try:
-                self.invert_index[t]
-            except KeyError:
-                self.invert_index[t]+=1
-            try:
-                self.tf[t,dj] += 1
-            except KeyError:
-                self.tf[t,dj] = 1
-        max_freq_tok = max(self.tf.values())
-        self.tf = {key:self.tf[key,dj]/max_freq_tok for key,_ in self.tf}
+    def calc_tf(self):
+        files = os.listdir(self.corpus_path)
+        for dj,file in enumerate(files):
+            if os.path.isfile(os.path.join(self.corpus_path, file)):
+                tokens = self.doc_to_tokens(file)
+                
+                for t in tokens:
+                    try:
+                        self.invert_index[t]
+                    except KeyError:
+                        self.invert_index[t] = 1 + self.invert_index[t] if dj else 1
+
+                    try:
+                        self.doc_tf[t,dj] += 1
+                    except KeyError:
+                        self.doc_tf[t,dj] = 1
+
+                max_freq_tok = max(self.doc_tf.values())
+                self.doc_tf = {key:self.doc_tf[key,dj]/max_freq_tok for key,_ in self.doc_tf}
 
     def calc_idf(self):
         for t in self.invert_index:
@@ -59,4 +73,40 @@ class Vector_Space_Model:
     def calc_weights(self):
         for t in self.invert_index:
             for d in range(self.corpus_size):
-                self.wights[t,d] = self.tf[t,d] * self.idf[t]
+                self.doc_wights[t,d] = self.doc_tf[t,d] * self.idf[t]
+
+    def calc_query_tf(self,query):
+        for t in query:
+            try:
+                self.query_tf[t] += 1
+            except KeyError:
+                self.query_tf[t] = 1
+
+        max_freq_tok = max(self.query_tf.values())
+        self.query_tf = {key:self.query_tf[key]/max_freq_tok for key,_ in self.query_tf}
+
+
+    def calc_query_weights(self,alpha):
+        for t in self.query_tf:
+            idf = 0
+            try:
+                idf = self.idf[t]
+            except KeyError: 
+                continue
+            
+            self.query_wights[t] = (alpha + (1-alpha) * self.query_tf[t]) * idf
+
+    def similarity(self,threshold):
+        for dj in range(self.corpus_size):
+            vect_prod = 0
+            for t in self.query_wights:
+                vect_prod += self.doc_wights[t,dj] * self.query_wights[t]
+
+            norm_d = np.linalg.norm(list(self.doc_wights.values()))
+            norm_q = np.linalg.norm(list(self.query_wights.values()))
+
+            self.sim[dj] = vect_prod/(norm_d * norm_q)
+
+        return sorted(self.sim.items(),key=lambda kv:kv[1],reverse=True)[0:threshold]
+            
+
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-l = {('a',1):3,('b',2):2}
+import numpy as np
 
-l = {key:l[key,1]/4 for key,_ in l }
-print(l)
-# print(l.values()/max(l.values()))
+l = {3:110,1:14}
+l = sorted(l.items(),key=lambda kv:kv[1],reverse=True)
+
+# n = np.linalg.norm(list(l.values()))
+
+print(l[:10])
+
