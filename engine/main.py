33905141708700eb5512c64d431bfda4d181ce22
JCH97 <jc997hdez@gmail.com>
2022-05-12 18:20:48 -0400

    fix()[engine]: add cors policy. call Pipeline from main

--- engine/main.py
+++ engine/main.py
@@ -1,17 +1,41 @@
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse, FileResponse, HTMLResponse
+from pipeline import Pipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+pipeline = None
 
 
 @app.get("/", response_class=FileResponse)
 def read_index(request: Request):
+    pipeline = Pipeline('')
+    pipeline.start()
+
     path = 'static/index.html'
     return FileResponse(path)
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Optional[str] = None):
-#     return {"item_id": item_id, "q": q}
+
+@app.get("/query")
+def query_docs(value: str = ""):
+    pipeline.process_query(value)
+
+    return pipeline.retrive_docs()
