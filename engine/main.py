159d237a29565aee7d9f2db7429279319fa14108
Abelo <abelo08cruz@gamil.com>
2022-05-13 21:58:50 -0400

    added front stuff

--- engine/main.py
+++ engine/main.py
@@ -3,18 +3,19 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse, FileResponse, HTMLResponse
 from pipeline import Pipeline
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+
+
+class Item(BaseModel):
+    path: str
+
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8081",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,15 +28,12 @@ pipeline = None
 
 @app.get("/", response_class=FileResponse)
 def read_index(request: Request):
-    pipeline = Pipeline('')
+    pipeline = Pipeline(os.path.join(os.getcwd(), 'corpus'))
     pipeline.start()
-
     path = 'static/index.html'
     return FileResponse(path)
-
 
 @app.get("/query")
 def query_docs(value: str = ""):
     pipeline.process_query(value)
-
     return pipeline.retrive_docs()
