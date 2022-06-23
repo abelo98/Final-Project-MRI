d25cb13f4c6905d50511f469ab8f132fb9d6e6bd
JCH97 <jc997hdez@gmail.com>
2022-06-22 23:56:46 -0400

    fix(): backend - add new routes for feedback management.

--- engine/main.py
+++ engine/main.py
@@ -1,15 +1,18 @@
-from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse, FileResponse, HTMLResponse
-from core import Core
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from uuid import UUID
 import os
+from uuid import UUID
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+from core import Core
 
 
-class Item(BaseModel):
-    path: str
+class FeedbackBody(BaseModel):
+    type: int
+    query: str
 
 
 app = FastAPI()
@@ -24,8 +27,8 @@ app.add_middleware(
 
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 
-pipeline = Core(os.path.join(os.getcwd(), 'corpus'))
-pipeline.start()
+core = Core(os.path.join(os.getcwd(), 'corpus'))
+core.start()
 
 
 @app.get("/", response_class=FileResponse)
@@ -34,17 +37,34 @@ def read_index(request: Request):
     return FileResponse(path)
 
 
-@app.get("/query")
-def query_docs(value: str = ""):
-    q_w = pipeline.process_query(value)
-    file_paths_and_ids = pipeline.retrieve_id_docs(q_w)
-    s = pipeline.get_subjects(file_paths_and_ids)
-    response = pipeline.make_response(file_paths_and_ids, s)
-    print(s)
+@app.get("/vect/query")
+def query_vect_docs(value: str = ""):
+    core.load_vectorial_model()
+
+    q_w = core.vsm.process_query(value)
+    file_paths_and_ids = core.vsm.retrieve_id_docs(q_w)
+    s = core.get_subjects(file_paths_and_ids)
+    response = core.make_response(file_paths_and_ids, s)
+
+    return response
+
+
+@app.get("/bool/query")
+def query_bool_docs(value: str = ""):
+    core.load_boolean_model()
+
+    file_paths_and_ids_boolean = core.boolean_model.boolean_model_retrieve_docs(value)
+    subjects = core.get_subjects(file_paths_and_ids_boolean)
+    response = core.make_response(file_paths_and_ids_boolean, subjects)
 
     return response
 
 
 @app.get("/document/id/{id}")
 def read_file(id: str):
-    return pipeline.retrieve_doc(UUID(id))
+    return core.retrieve_doc(UUID(id))
+
+
+@app.put("/feedback/{doc_id}")
+def feedback(doc_id: int, body: FeedbackBody):
+    return core.set_feedback(body.type, doc_id, body.query)
---
+++ engine/static/js/app.9afc0177.js
@@ -0,0 +1,345 @@
+(function (e) {
+    function t(t) {
+        for (var r, a, s = t[0], i = t[1], u = t[2], p = 0, d = []; p < s.length; p++) a = s[p], Object.prototype.hasOwnProperty.call(o, a) && o[a] && d.push(o[a][0]), o[a] = 0;
+        for (r in i) Object.prototype.hasOwnProperty.call(i, r) && (e[r] = i[r]);
+        l && l(t);
+        while (d.length) d.shift()();
+        return c.push.apply(c, u || []), n()
+    }
+
+    function n() {
+        for (var e, t = 0; t < c.length; t++) {
+            for (var n = c[t], r = !0, s = 1; s < n.length; s++) {
+                var i = n[s];
+                0 !== o[i] && (r = !1)
+            }
+            r && (c.splice(t--, 1), e = a(a.s = n[0]))
+        }
+        return e
+    }
+
+    var r = {}, o = {app: 0}, c = [];
+
+    function a(t) {
+        if (r[t]) return r[t].exports;
+        var n = r[t] = {i: t, l: !1, exports: {}};
+        return e[t].call(n.exports, n, n.exports, a), n.l = !0, n.exports
+    }
+
+    a.m = e, a.c = r, a.d = function (e, t, n) {
+        a.o(e, t) || Object.defineProperty(e, t, {enumerable: !0, get: n})
+    }, a.r = function (e) {
+        "undefined" !== typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {value: "Module"}), Object.defineProperty(e, "__esModule", {value: !0})
+    }, a.t = function (e, t) {
+        if (1 & t && (e = a(e)), 8 & t) return e;
+        if (4 & t && "object" === typeof e && e && e.__esModule) return e;
+        var n = Object.create(null);
+        if (a.r(n), Object.defineProperty(n, "default", {
+            enumerable: !0,
+            value: e
+        }), 2 & t && "string" != typeof e) for (var r in e) a.d(n, r, function (t) {
+            return e[t]
+        }.bind(null, r));
+        return n
+    }, a.n = function (e) {
+        var t = e && e.__esModule ? function () {
+            return e["default"]
+        } : function () {
+            return e
+        };
+        return a.d(t, "a", t), t
+    }, a.o = function (e, t) {
+        return Object.prototype.hasOwnProperty.call(e, t)
+    }, a.p = "/";
+    var s = window["webpackJsonp"] = window["webpackJsonp"] || [], i = s.push.bind(s);
+    s.push = t, s = s.slice();
+    for (var u = 0; u < s.length; u++) t(s[u]);
+    var l = i;
+    c.push([0, "chunk-vendors"]), n()
+})({
+    0: function (e, t, n) {
+        e.exports = n("56d7")
+    }, "034f": function (e, t, n) {
+        "use strict";
+        n("85ec")
+    }, 2799: function (e, t, n) {
+        "use strict";
+        n("c65d")
+    }, "33a2": function (e, t, n) {
+        "use strict";
+        n("3d35")
+    }, "3d35": function (e, t, n) {
+    }, 4651: function (e, t, n) {
+        "use strict";
+        n("a34e")
+    }, "51f2": function (e, t, n) {
+        e.exports = n.p + "static/img/search.5d247d74.svg"
+    }, "56d7": function (e, t, n) {
+        "use strict";
+        n.r(t);
+        n("e260"), n("e6cf"), n("cca6"), n("a79d");
+        var r = n("2b0e"), o = n("8c4f"), c = function () {
+                var e = this, t = e.$createElement, n = e._self._c || t;
+                return n("div", {staticClass: "home-container"}, [n("img", {attrs: {src: e.logo}}), n("SearchInput", {
+                    ref: "searchInput",
+                    on: {submit: e.onSubmit}
+                }), n("button", {on: {click: e.handleSearch}}, [e._v("Buscar")])], 1)
+            }, a = [], s = (n("d3b7"), n("e9c4"), function () {
+                var e = this, t = e.$createElement, n = e._self._c || t;
+                return n("div", {staticClass: "search"}, [e.fromSearch ? e._e() : n("img", {attrs: {src: e.searchImg}}), n("input", {
+                    directives: [{
+                        name: "model",
+                        rawName: "v-model",
+                        value: e.value,
+                        expression: "value"
+                    }], domProps: {value: e.value}, on: {
+                        keydown: function (t) {
+                            return !t.type.indexOf("key") && e._k(t.keyCode, "enter", 13, t.key, "Enter") ? null : e.onSubmit.apply(null, arguments)
+                        }, input: function (t) {
+                            t.target.composing || (e.value = t.target.value)
+                        }
+                    }
+                }), e.fromSearch ? n("img", {
+                    staticStyle: {"margin-left": "10px", cursor: "pointer"},
+                    attrs: {src: e.searchImg},
+                    on: {click: e.onSubmit}
+                }) : e._e()])
+            }), i = [], u = {
+                props: {fromSearch: {type: Boolean, default: !1}}, data: function () {
+                    return {searchImg: n("51f2"), value: ""}
+                }, methods: {
+                    onSubmit: function () {
+                        this.$emit("submit", this.value)
+                    }, setValue: function (e) {
+                        this.value = e
+                    }
+                }
+            }, l = u, p = (n("33a2"), n("2877")), d = Object(p["a"])(l, s, i, !1, null, "1125ec08", null), f = d.exports,
+            m = {
+                components: {SearchInput: f}, data: function () {
+                    return {logo: n("cf05")}
+                }, methods: {
+                    handleSearch: function () {
+                        var e = this.$refs["searchInput"];
+                        this.onSubmit(e.value)
+                    }, corpusRoute: function () {
+                        var e = this.$refs["corpus"];
+                        this.sendCorpus(e.value)
+                    }, sendCorpus: function (e) {
+                        fetch("http://127.0.0.1:8000/corpus", {
+                            method: "post",
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Access-Control-Allow-Origin": "*",
+                                Accept: "application/json"
+                            },
+                            body: JSON.stringify({path: e})
+                        })
+                    }, onSubmit: function (e) {
+                        "" !== e && this.$router.push({name: "search", query: {value: e}})
+                    }
+                }
+            }, h = m, v = (n("2799"), Object(p["a"])(h, c, a, !1, null, "35b415c3", null)), g = v.exports,
+            b = function () {
+                var e = this, t = e.$createElement, n = e._self._c || t;
+                return n("div", {staticClass: "search-container"}, [e.loading ? n("div", {staticClass: "loader"}, [n("img", {attrs: {src: e.searchLoad}})]) : e._e(), n("div", {staticClass: "header"}, [n("img", {
+                    staticStyle: {cursor: "pointer"},
+                    attrs: {src: e.logo},
+                    on: {click: e.goToHome}
+                }), n("SearchInput", {
+                    ref: "searchInput",
+                    attrs: {"from-search": ""},
+                    on: {submit: e.onSubmit}
+                }), n("input", {
+                    directives: [{
+                        name: "model",
+                        rawName: "v-model",
+                        value: e.vectorialModel,
+                        expression: "vectorialModel"
+                    }],
+                    staticStyle: {"margin-left": "20px"},
+                    attrs: {type: "checkbox"},
+                    domProps: {checked: Array.isArray(e.vectorialModel) ? e._i(e.vectorialModel, null) > -1 : e.vectorialModel},
+                    on: {
+                        change: function (t) {
+                            var n = e.vectorialModel, r = t.target, o = !!r.checked;
+                            if (Array.isArray(n)) {
+                                var c = null, a = e._i(n, c);
+                                r.checked ? a < 0 && (e.vectorialModel = n.concat([c])) : a > -1 && (e.vectorialModel = n.slice(0, a).concat(n.slice(a + 1)))
+                            } else e.vectorialModel = o
+                        }
+                    }
+                }), e._v(" Use Vectorial Model ")], 1), n("div", {staticClass: "body"}, e._l(e.documents, (function (t) {
+                    return n("div", {
+                        key: t.id,
+                        staticClass: "document-retrieve"
+                    }, [n("div", {staticClass: "content"}, [n("div", {staticClass: "text-content"}, [n("router-link", {
+                        attrs: {
+                            to: {
+                                name: "document",
+                                params: {id: t.id}
+                            }, target: "_blank"
+                        }, on: {
+                            click: function (n) {
+                                return n.preventDefault(), e.openDocument(t.id)
+                            }
+                        }
+                    }, [e._v(" Open document ")]), n("p", {staticClass: "text-preview"}, [e._v(e._s(t.subject))])], 1)]), n("div", {staticClass: "doc-metrics"}, [n("img", {
+                        staticClass: "like",
+                        attrs: {src: e.likeImg},
+                        on: {
+                            click: function (n) {
+                                return e.setFeedback(t.id, 1)
+                            }
+                        }
+                    }), n("p", {staticClass: "ranking"}, [e._v("-")]), n("img", {
+                        staticClass: "dislike",
+                        attrs: {src: e.dislikeImg},
+                        on: {
+                            click: function (n) {
+                                return e.setFeedback(t.id, -1)
+                            }
+                        }
+                    })])])
+                })), 0)])
+            }, y = [], x = n("1da1"), k = (n("96cf"), {
+                components: {SearchInput: f}, data: function () {
+                    return {
+                        loading: !1,
+                        searchLoad: n("fac4"),
+                        logo: n("cf05"),
+                        likeImg: n("c8e1"),
+                        dislikeImg: n("8148"),
+                        documents: [],
+                        vectorialModel: !0
+                    }
+                }, mounted: function () {
+                    var e = this.$route.query.value;
+                    this.getDocuments(e), this.$refs["searchInput"].setValue(e)
+                }, methods: {
+                    goToHome: function () {
+                        this.$router.push({name: "home"})
+                    }, onSubmit: function (e) {
+                        "" !== e && (this.$router.push({name: "search", query: {value: e}}), this.getDocuments(e))
+                    }, getDocuments: function (e) {
+                        var t = this;
+                        return Object(x["a"])(regeneratorRuntime.mark((function n() {
+                            var r, o, c;
+                            return regeneratorRuntime.wrap((function (n) {
+                                while (1) switch (n.prev = n.next) {
+                                    case 0:
+                                        return t.loading = !0, n.prev = 1, r = t.vectorialModel ? "http://127.0.0.1:8000/vect/query?value=".concat(e) : "http://127.0.0.1:8000/bool/query?value=".concat(e), n.next = 5, fetch(r);
+                                    case 5:
+                                        return o = n.sent, n.t0 = JSON, n.next = 9, o.text();
+                                    case 9:
+                                        n.t1 = n.sent, c = n.t0.parse.call(n.t0, n.t1), t.documents = c, n.next = 17;
+                                        break;
+                                    case 14:
+                                        n.prev = 14, n.t2 = n["catch"](1), console.log("Error ".concat(n.t2));
+                                    case 17:
+                                        t.loading = !1;
+                                    case 18:
+                                    case"end":
+                                        return n.stop()
+                                }
+                            }), n, null, [[1, 14]])
+                        })))()
+                    }, setFeedback: function (e, t) {
+                        return Object(x["a"])(regeneratorRuntime.mark((function n() {
+                            var r, o, c, a;
+                            return regeneratorRuntime.wrap((function (n) {
+                                while (1) switch (n.prev = n.next) {
+                                    case 0:
+                                        return n.prev = 0, r = new Headers, r.append("Content-type", "application/json"), o = JSON.stringify({
+                                            type: t,
+                                            query: ""
+                                        }), n.next = 6, fetch("http://127.0.0.1:8000/feedback/".concat(e), {
+                                            method: "PUT",
+                                            body: o,
+                                            headers: r
+                                        });
+                                    case 6:
+                                        return c = n.sent, n.t0 = JSON, n.next = 10, c.text();
+                                    case 10:
+                                        n.t1 = n.sent, a = n.t0.parse.call(n.t0, n.t1), console.log(a), n.next = 18;
+                                        break;
+                                    case 15:
+                                        n.prev = 15, n.t2 = n["catch"](0), console.log(n.t2);
+                                    case 18:
+                                    case"end":
+                                        return n.stop()
+                                }
+                            }), n, null, [[0, 15]])
+                        })))()
+                    }, openDocument: function (e) {
+                        this.$router.push({name: "document", params: {id: e}})
+                    }
+                }
+            }), _ = k, S = (n("cdd2"), Object(p["a"])(_, b, y, !1, null, "634eea7f", null)), w = S.exports,
+            O = function () {
+                var e = this, t = e.$createElement, n = e._self._c || t;
+                return n("div", {staticClass: "body"}, [e._v(" " + e._s(e.text) + " ")])
+            }, j = [], C = {
+                data: function () {
+                    return {text: ""}
+                }, created: function () {
+                    var e = this.$route.params.id;
+                    this.getDocument(e)
+                }, methods: {
+                    getDocument: function (e) {
+                        var t = this;
+                        return Object(x["a"])(regeneratorRuntime.mark((function n() {
+                            var r, o;
+                            return regeneratorRuntime.wrap((function (n) {
+                                while (1) switch (n.prev = n.next) {
+                                    case 0:
+                                        return n.prev = 0, n.next = 3, fetch("http://127.0.0.1:8000/document/".concat(e));
+                                    case 3:
+                                        return r = n.sent, n.t0 = JSON, n.next = 7, r.text();
+                                    case 7:
+                                        n.t1 = n.sent, o = n.t0.parse.call(n.t0, n.t1), t.text = o.text, n.next = 15;
+                                        break;
+                                    case 12:
+                                        n.prev = 12, n.t2 = n["catch"](0), console.log(n.t2);
+                                    case 15:
+                                    case"end":
+                                        return n.stop()
+                                }
+                            }), n, null, [[0, 12]])
+                        })))()
+                    }
+                }
+            }, M = C, $ = (n("4651"), Object(p["a"])(M, O, j, !1, null, "14201b30", null)), I = $.exports;
+        r["a"].use(o["a"]);
+        var P = [{path: "/", name: "home", component: g}, {
+                path: "/search",
+                name: "search",
+                component: w
+            }, {path: "/document/id/:id", name: "document", component: I}, {path: "*", redirect: {name: "home"}}],
+            D = new o["a"]({mode: "history", routes: P}), A = D, E = function () {
+                var e = this, t = e.$createElement, n = e._self._c || t;
+                return n("div", {attrs: {id: "app"}}, [n("router-view")], 1)
+            }, J = [], N = (n("034f"), {}), R = Object(p["a"])(N, E, J, !1, null, null, null), T = R.exports;
+        r["a"].config.productionTip = !1, new r["a"]({
+            router: A, render: function (e) {
+                return e(T)
+            }
+        }).$mount("#app")
+    }, 8148: function (e, t, n) {
+        e.exports = n.p + "static/img/dislike.89c298f0.svg"
+    }, "85ec": function (e, t, n) {
+    }, "9f67": function (e, t, n) {
+    }, a34e: function (e, t, n) {
+    }, c65d: function (e, t, n) {
+    }, c8e1: function (e, t, n) {
+        e.exports = n.p + "static/img/like.9b4e4d46.svg"
+    }, cdd2: function (e, t, n) {
+        "use strict";
+        n("9f67")
+    }, cf05: function (e, t, n) {
+        e.exports = n.p + "static/img/logo.13a68a62.png"
+    }, fac4: function (e, t, n) {
+        e.exports = n.p + "static/img/search_load.787e700c.svg"
+    }
+});
+//# sourceMappingURL=app.9afc0177.js.map
