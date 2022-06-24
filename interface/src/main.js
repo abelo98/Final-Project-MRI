7d894401745eef7bf1f0621b7f6248887d6bd32d
JCH97 <jc997hdez@gmail.com>
2022-06-24 12:03:35 -0400

    feat(): frontend - initial approach for management several corpus

--- interface/src/main.js
+++ interface/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import router from "./router/router.js";
 import App from "./App.vue"
+import vSelect from 'vue-select'
 
 Vue.config.productionTip = false;
+Vue.component('v-select', vSelect)
 
 new Vue({
   router,
--- interface/src/router/router.js
+++ interface/src/router/router.js
@@ -13,7 +13,7 @@ const routes = [
     component: Home,
   },
   {
-    path: "/search",
+    path: "/search/:corpus",
     name: "search",
     component: Search,
   },
