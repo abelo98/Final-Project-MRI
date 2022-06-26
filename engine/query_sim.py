86246847f21fd4bfb4fc63eb03128aef2f3b9bd2
Abelo <abelo08cruz@gamil.com>
2022-06-26 17:59:06 -0400

    fixed term_coincidence variable

--- engine/query_sim.py
+++ engine/query_sim.py
@@ -5,33 +5,27 @@ class query_processor:
     def __init__(self) -> None:
         self.query_historial = {}
 
-    def save_query(self, q, vector_q: dict):
-        self.query_historial[str(q)] = (vector_q, np.linalg.norm(list(vector_q.values())))
-
+    def save_query(self,q,vector_q:dict):
+        if not q in self.query_historial.keys():
+            self.query_historial[q] = (vector_q,np.linalg.norm(list(vector_q.values())))
+    
     # q is a tuple formed by q[0] = dict (reresenting the vector of the query) 
     # and q[1] is the norm of the vect
-    def similar(self, vector_q, k=4):
+    def similar(self,q_original,vector_q,k = 4):
         sim = np.zeros(k)
-        queries_result = [None] * k
-        vect_prod = 0
+        queries_result = [None]*k
 
-        for query_str, vector_q_saved_and_norm in self.query_historial.items():
+        for query_str ,vector_q_saved_and_norm in self.query_historial.items():
+            vect_prod = 0
+            term_coincidences = 0
+            if q_original == query_str:continue
             for t in vector_q:
                 try:
                     vect_prod += vector_q_saved_and_norm[0][t] * vector_q[t]
+                    term_coincidences += 1 
                 except KeyError:
                     pass
-            norm_q1 = vector_q_saved_and_norm[1]
-            norm_q2 = np.linalg.norm(list(vector_q.values()))
-            norm_p = norm_q1 * norm_q2
-            if norm_p and vect_prod:
-                s = vect_prod / norm_p
-                if s > sim.min():
-                    index = sim.argmin()
-                    sim[index] = s
-                    queries_result[index] = query_str
 
-<<<<<<< HEAD
             if (term_coincidences*100) /len(vector_q) >= 30:
                 norm_q1 = vector_q_saved_and_norm[1]
                 norm_q2 = np.linalg.norm(list(vector_q.values()))
@@ -43,6 +37,4 @@ class query_processor:
                         sim[index] = s
                         queries_result[index] = query_str
                         
-=======
->>>>>>> 3f87bbd4937b42545b864114705799997135ef04
         return queries_result
